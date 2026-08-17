19c448c09553de414d945c43a1bf857e63b98d4f
atsumaxxx-eng <atsu.maxxx@gmail.com>
2026-08-17 19:14:51 +0800

    Cowork deploy 2026/08/17 19:14:51.58

--- yuwaku/sw.js
+++ yuwaku/sw.js
@@ -1,6 +1,6 @@
 // Service Worker: アプリシェルをキャッシュし、オフラインでも起動できるようにする。
 // API（script.google.com）へのPOSTは横取りせず素通しする。
-const CACHE = 'yuwaku-pos-v120';
+const CACHE = 'yuwaku-pos-v121';
 const SHELL = [
   './',
   './index.html',
